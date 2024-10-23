6b109e479d541f4eb47e553b7ed1f4fda28a04b1
aidazylfo <aida.zylfo@ada.ac.uk>
2024-10-23 09:59:27 +0000

    assigment

--- dfp.js
+++ dfp.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 
 function parseFile (indata, outdata, delimiter = ';') {
-
+  if (!fs.existsSync(indata)) { //existsSync checks if the data (input data) does actually exist 
+    return -1; //returns -1 if data exists
+  }
+  if (fs.existsSync(outdata)) {
+    fs.unlinkSync(outdata); //if output file does already exist then its deleted using fs.unlinksync
+  }
+ const data = fs.readFileSync(indata, 'utf-8')
+ const lines = data.split('\n');
+ const transformedLines = [];
+ let totalRecordsExported = 0;
 }
 
 
@@ -13,4 +22,16 @@ function parseFile (indata, outdata, delimiter = ';') {
 // Leave this code here for the automated tests
 module.exports = {
   parseFile,
+}
+
+
+
+
+// possible usefull code 
+function splitTextIntoChunks (text, chunksize = 20) {
+  let chunks = [];
+  for (let i = 0; i < text.length; i += chunkSize) {
+    chunks.push(text.slice(i, i + chunkSize));
+  }
+ return chunks;
 }
